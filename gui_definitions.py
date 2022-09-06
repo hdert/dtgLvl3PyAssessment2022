a152dfdc90c31c66291c787c7820b4dc5127daa3
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2022-09-06 20:05:17 +1200

    add basic definition for a GUI

---
+++ gui_definitions.py
@@ -0,0 +1,9 @@
+"""Defining the GUI, GUI elements, and GUI related variables."""
+from tkinter import Tk
+# from tkinter import Tk, ttk, Label, StringVar, Frame, Entry, Button,
+# LabelFrame, PhotoImage, ComboBox, DoubleVar
+
+root = Tk()
+root.title("Placeholder Text")
+
+root.mainloop()
